39fe4ea1267738cff5abb0d6b135d4efdc91e432
kelly6bf <kelly6bf@gmail.com>
2024-07-12 20:50:07 +0900

    2중 loop 풀이 코드 수정

--- Sec02/Two Sum/Solution01.java
+++ Sec02/Two Sum/Solution01.java
@@ -1,24 +1,17 @@
-/**
- * 풀이법 01 : 2중 iter 방식
- */
-
 public class Solution01 {
-
     public int[] twoSum(int[] nums, int target) {
-        int numsCount = nums.length;
-
-        for(int firstIndex = 0; firstIndex < numsCount - 1; firstIndex++) {
-            for (int secondIndex = firstIndex + 1; secondIndex < numsCount; secondIndex++) {
-                if (checkSum(target, nums[firstIndex], nums[secondIndex])) {
-                    return new int[]{firstIndex, secondIndex};
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (equalSumValueToTarget(target, nums[i], nums[j])) {
+                    return new int[]{i, j};
                 }
             }
         }
 
-        return new int[]{0, 0};
+        return new int[2];
     }
 
-    private boolean checkSum(int target, int num1, int num2) {
+    private boolean equalSumValueToTarget(int target, int num1, int num2) {
         return num1 + num2 == target;
     }
 }
